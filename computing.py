fd8e5adb450d745333da8e17ec2e030b0d3232dc
Lukas <moilukasdu85@gmail.com>
2024-01-17 23:58:09 +0100

    colors and that's all I think

--- computing.py
+++ computing.py
@@ -7,7 +7,7 @@ def displayWalls(d, screen):
     numbOfSlices = len(d)
     walls = [pygame.Surface((math.ceil(screen.get_width()/numbOfSlices),2*800/d[i])) for i in range(numbOfSlices)]
     for wall in walls:
-        wall.fill("#FF0000")
+        wall.fill("#8B4009")
     return walls
 
 #rayPart
@@ -38,22 +38,23 @@ matMap=[[1,1,1,1,1,1,1,1,1,1],
         [1,0,0,0,0,0,0,0,0,1],
         [1,1,1,1,1,1,1,1,1,1]]
 
-playerPosX = 1.1
-playerPosY = 1.1
+playerPosX = 2
+playerPosY = 2
 dx = [0,0]
 dy = [0,0]
 theta = 0   #where do you watch ? 0 = right, 90 = down, 180 = left, 270 = -90 = top
 dt = 0
 initSpeed = 0.1
 speed = initSpeed
+run = 1
 circSpeed = 2
 
 playerPosZ = 0
 heightVisu = 0
 
-fov = 60    #if next to a wall and fov of 90, not working
+fov = 60
 fov = fov/2
-numbOfRays = 101
+numbOfRays = 100
 cst = 2*fov/(numbOfRays-1)
 thetas = [(theta-fov+cst*i)*math.pi/180 for i in range(numbOfRays)]
 theta2 = theta*math.pi/180
@@ -68,8 +69,10 @@ screen = pygame.display.set_mode((width,height))
 pygame.display.set_caption("Doom_like")
 clock = pygame.time.Clock()
 
-bg = pygame.Surface((width,height))
-bg.fill("#000000")
+bgTop = pygame.Surface((width,height))
+bgTop.fill("#00F3FF")
+bgBot = pygame.Surface((width,height))
+bgBot.fill("#10AF08")
 
 walls = displayWalls(d, screen)
 
@@ -99,9 +102,9 @@ while True:
                 dx = [dx[0]+1,dx[1]]
                 dy = [dy[0],dy[1]-1]
             if event.key == pygame.K_RIGHT :
-                dt = circSpeed
-            elif event.key == pygame.K_LEFT :
-                dt = -circSpeed
+                dt += circSpeed
+            if event.key == pygame.K_LEFT :
+                dt -= circSpeed
             if event.key == pygame.K_LCTRL :
                 if playerPosZ == 0:
                     playerPosZ = -96
@@ -112,10 +115,14 @@ while True:
             elif event.key == pygame.K_SPACE :
                 if playerPosZ == 0 and heightVisu == 0:
                     playerPosZ = 420
+            if event.key == pygame.K_LSHIFT :
+                run *= 2
                 
         elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_RIGHT or event.key == pygame.K_LEFT :
-                dt = 0
+            if event.key == pygame.K_RIGHT :
+                dt -= circSpeed
+            if event.key == pygame.K_LEFT :
+                dt += circSpeed
             if event.key == pygame.K_w:
                 dx = [dx[0],dx[1]-1]
                 dy = [dy[0]-1,dy[1]]
@@ -128,12 +135,15 @@ while True:
             if event.key == pygame.K_d:
                 dx = [dx[0]-1,dx[1]]
                 dy = [dy[0],dy[1]+1]
+            if event.key == pygame.K_LSHIFT :
+                run /= 2
     
     theta += dt
-    speedCos = speed*math.cos(theta2)
-    speedSin = speed*math.sin(theta2)
-    if matMap[math.floor(playerPosX + dx[0]*speedCos+dx[1]*speedSin)][math.floor(playerPosY + dy[0]*speedCos+dy[1]*speedSin)]!=1:
+    speedCos = run*speed*math.cos(theta2)
+    speedSin = run*speed*math.sin(theta2)
+    if matMap[math.floor(playerPosX + dx[0]*speedCos+dx[1]*speedSin)][math.floor(playerPosY)]!=1:
         playerPosX+= dx[0]*speedCos+dx[1]*speedSin
+    if matMap[math.floor(playerPosX)][math.floor(playerPosY + dy[0]*speedCos+dy[1]*speedSin)]!=1:
         playerPosY+= dy[0]*speedCos+dy[1]*speedSin
     if playerPosZ > 0:
         playerPosZ -= 14
@@ -143,7 +153,8 @@ while True:
         heightVisu -= 8
     
     #print screen
-    screen.blit(bg, (0,0))
+    screen.blit(bgTop, (0,-screen.get_height()//2+ heightVisu))
+    screen.blit(bgBot, (0,screen.get_height()//2+ heightVisu))
     for i in range(len(walls)):
         x = i*screen.get_width()/len(walls)
         y = screen.get_height()//2 - walls[i].get_height() // 2 + heightVisu
