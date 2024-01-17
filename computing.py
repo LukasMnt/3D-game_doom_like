1de44ff26180e3ec3c7dd0198ad6f5e9740acc93
Lukas <moilukasdu85@gmail.com>
2024-01-17 18:08:52 +0100

    first file for the map and rays and player infos (empty)

---
+++ computing.py
@@ -0,0 +1,23 @@
+import math
+
+def distanceR(playerPos, matMap):
+    distance = 0
+    return distance
+    
+
+matMap=[[1,1,1,1,1,1,1,1,1,1],
+        [1,0,0,0,0,0,0,0,0,1],
+        [1,0,0,0,0,0,0,0,0,1],
+        [1,0,0,0,0,0,0,0,0,1],
+        [1,0,0,0,0,0,0,0,0,1],
+        [1,0,0,0,0,0,0,0,0,1],
+        [1,0,0,0,0,0,0,0,0,1],
+        [1,0,0,0,0,0,0,0,0,1],
+        [1,0,0,0,0,0,0,0,0,1],
+        [1,1,1,1,1,1,1,1,1,1]]
+
+playerPos = (1,1)
+theta = 0
+
+#premier mur a disatance de 8
+d = distanceR(playerPos, matMap)
