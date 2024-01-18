db4fde5e6ce9e441adfac77b794aa0d255afcb47
Lukas <moilukasdu85@gmail.com>
2024-01-18 14:03:08 +0100

    changed the way to compute rays + added a crosshair

--- computing.py
+++ computing.py
@@ -13,16 +13,19 @@ def displayWalls(d, screen):
 #rayPart
 def distanceR(playerPosX, playerPosY, matMap, thetas, numbOfRays):
     i=0
-    distances = [0 for i in range(numbOfRays)]
+    rayImp = 0.01
+    distances = [0.1 for i in range(numbOfRays)]
     for theta in thetas:
         isWall = False
+        xUp = rayImp*math.cos(theta)
+        yUp = rayImp*math.sin(theta)
+        watchX = playerPosY
+        watchY = playerPosX
         while not isWall :
-            distances[i] += 0.01
-            watchX = playerPosY+distances[i]*math.cos(theta)
-            watchY = playerPosX+distances[i]*math.sin(theta)
-            watchX = math.floor(watchX)
-            watchY = math.floor(watchY)
-            if matMap[watchY][watchX] == 1:
+            distances[i] += rayImp
+            watchX += xUp
+            watchY += yUp
+            if matMap[math.floor(watchY)][math.floor(watchX)] == 1:
                 isWall = True
         i+=1
     return distances
@@ -51,19 +54,19 @@ playerPosX = 2
 playerPosY = 2
 dx = [0,0]
 dy = [0,0]
-theta = 0   #where do you watch ? 0 = right, 90 = down, 180 = left, 270 = -90 = top
+theta = 45   #where do you watch ? 0 = right, 90 = down, 180 = left, 270 = -90 = top
 dt = 0
-initSpeed = 0.05
+initSpeed = 0.03
 speed = initSpeed
 run = 1
-circSpeed = 2
+circSpeed = 3
 
 playerPosZ = 0
 heightVisu = 0
 
 fov = 60
 fov = fov/2
-numbOfRays = 100
+numbOfRays = 101
 cst = 2*fov/(numbOfRays-1)
 thetas = [(theta-fov+cst*i)*math.pi/180 for i in range(numbOfRays)]
 theta2 = theta*math.pi/180
@@ -82,6 +85,11 @@ bgTop = pygame.Surface((width,height))
 bgTop.fill("#00F3FF")
 bgBot = pygame.Surface((width,height))
 bgBot.fill("#10AF08")
+
+crosshairV = pygame.Surface((2,16))
+crosshairV.fill("#FF0000")
+crosshairH = pygame.Surface((16,2))
+crosshairH.fill("#FF0000")
 
 walls = displayWalls(d, screen)
 
@@ -150,9 +158,9 @@ while True:
     theta += dt
     speedCos = run*speed*math.cos(theta2)
     speedSin = run*speed*math.sin(theta2)
-    if matMap[math.floor(playerPosX + dx[0]*speedCos+dx[1]*speedSin)][math.floor(playerPosY)]!=1:
+    if matMap[math.floor(playerPosX + dx[0]*speedCos+dx[1]*speedSin)][math.floor(playerPosY)]!=1 :
         playerPosX+= dx[0]*speedCos+dx[1]*speedSin
-    if matMap[math.floor(playerPosX)][math.floor(playerPosY + dy[0]*speedCos+dy[1]*speedSin)]!=1:
+    if matMap[math.floor(playerPosX)][math.floor(playerPosY + dy[0]*speedCos+dy[1]*speedSin)]!=1 :
         playerPosY+= dy[0]*speedCos+dy[1]*speedSin
     if playerPosZ > 0:
         playerPosZ -= 14
@@ -168,6 +176,9 @@ while True:
         x = i*screen.get_width()/len(walls)
         y = screen.get_height()//2 - walls[i].get_height() // 2 + heightVisu
         screen.blit(walls[i], (x,y))
+    
+    screen.blit(crosshairV, ((screen.get_width()-crosshairV.get_width())//2,(screen.get_height()-crosshairV.get_height())//2))
+    screen.blit(crosshairH, ((screen.get_width()-crosshairH.get_width())//2,(screen.get_height()-crosshairH.get_height())//2))
     
     pygame.display.update()
     clock.tick(60)
